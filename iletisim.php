4c904c129d1100fc8e35d07a3ab4ed2d3c4d1f3c
elya-gif <yagmurealtun@gmail.com>
2026-02-24 21:13:18 +0300

    Update iletisim.php

--- iletisim.php
+++ iletisim.php
@@ -3,7 +3,7 @@ include "header.php";
 ?>
 
 <div class="container mt-5">
-    <h1>Merhaba Bootstrap + jQuery</h1>
+    <h1>Merhaba Bootstrap + jkhjkhkhjhjkkhjjhkhjkkhj</h1>
 </div>
 
 <?php include "footer.php"; ?>
