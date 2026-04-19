d9769157da5f00daad0f68af25eb6b5cff2fe426
ılgın <ilginyenigun@gmail.com>
2026-04-19 20:01:20 +0300

    yeni

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+try {
+    $pdo = new PDO(
+        'mysql:host=127.0.0.1;port=8889;dbname=megay_moda;charset=utf8',
+        'root',
+        'root'
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Bağlantı hatası: ' . $e->getMessage());
+}
+?>
